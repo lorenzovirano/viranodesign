482472f2e322df043c18a3cd193d1f60f90f7edb
Lorenzo Virano <lorenzo.virano@meetweb.it>
2022-11-22 15:00:11 +0100

    Stile header e giignore

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,24 @@
+import css from '../styles/Header.module.css';
+
 export default function Header(){
     return(
-        <header>
-            header
+        <header className={css.header}>
+            <div className={css.header__left}>
+                <figure height={30}>logo</figure>
+                <ul className={css.navbar}>
+                    <li className={css.navbar__item}><a href="#">Item 1</a></li>
+                    <li className={css.navbar__item}><a href="#">Item 2</a></li>
+                    <li className={css.navbar__item}><a href="#">Item 3</a></li>
+                    <li className={css.navbar__item}><a href="#">Item 4</a></li>
+                </ul>
+            </div>
+            <div className={css.header__right}>
+                <div className={css.download__btn}>
+                    <a href="">
+                        Download CV
+                    </a>
+                </div>
+            </div>
         </header>
     )
 }
