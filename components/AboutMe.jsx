cb96e438a1f1f9b01c584e1b0146929981e76802
Lorenzo Virano <lorenzo.virano@meetweb.it>
2023-02-16 13:36:52 +0100

    Creata sezione about me

--- components/AboutMe.jsx
+++ components/AboutMe.jsx
@@ -4,7 +4,12 @@ export default function AboutMe() {
     let sectionClasses = `section section__about-me`
     return (
         <section className={sectionClasses}>
-            <h1 className={css.section__title}>About Me</h1>
+            <h1 className="section__title">About Me</h1>
+            <div className='text-container'>
+                <p>
+                {`Ciao, sono Lorenzo Virano, uno sviluppatore frontend junior di 20 anni. Sono competente nella creazione e gestione di database, nonché nello sviluppo frontend e backend. Al momento sto frequentando il secondo anno di università di informatica presso l'Università Parthenope di Napoli. Sono appassionato di tecnologia e mi piace esplorare nuove tecnologie e acquisire nuove competenze. Nonostante la mia posizione di junior, sono entusiasta e proattivo nel mio lavoro, e sono sempre alla ricerca di nuove opportunità per imparare e crescere professionalmente. Sono una persona creativa e mi piace contribuire con idee innovative.`}
+                </p>
+            </div>
         </section>
     )
 }
--- components/Header.jsx
+++ components/Header.jsx
@@ -3,8 +3,11 @@ import css from '../styles/Header.module.css';
 export default function Header(){
     return(
         <header className={css.header}>
+            <div className="container container--header">
             <div className={css.header__left}>
-                <figure height={30}>logo</figure>
+                <figure height={30}>
+                    Logo
+                </figure>
                 <ul className={css.navbar}>
                     <li className={css.navbar__item}><a href="#">Item 1</a></li>
                     <li className={css.navbar__item}><a href="#">Item 2</a></li>
@@ -18,6 +21,7 @@ export default function Header(){
                         Download CV
                     </a>
                 </div>
+            </div>
             </div>
         </header>
     )
