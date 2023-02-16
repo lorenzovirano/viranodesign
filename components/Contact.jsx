5e561905e1253cfe8bdf56c12f6a0376909fa081
Lorenzo Virano <lorenzo.virano@meetweb.it>
2023-02-16 17:55:45 +0100

    Creata section contatti

---
+++ components/Contact.jsx
@@ -0,0 +1,12 @@
+import css from '../styles/Contact.module.css'
+
+export default function Contact() {
+    let sectionClasses = `section section__projects`
+    return (
+        <section className={sectionClasses}>
+            <h1 className="section__title title--right">Contact</h1>
+            <div className='content'>
+            </div>
+        </section>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import styles from '../styles/Home.module.css'
 import AboutMe from "../components/AboutMe";
 import Skills from "../components/Skills";
 import Projects from "../components/Projects";
+import Contact from "../components/Contact";
 import { Container } from "react-bootstrap";
 
 export default function Home() {
@@ -16,6 +17,7 @@ export default function Home() {
         <AboutMe />
         <Skills />
         <Projects />
+        <Contact />
       </Container>
     </Layout>
   )
